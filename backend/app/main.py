a74f6a81c7ae1ceb95554ad2966b112c718aad35
mrubin01 <mi.rubino@protonmail.com>
2025-12-13 09:56:20 +0000

    Initialize the db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
-pass
+from fastapi import FastAPI
+from app.db.database import Base, engine
+
+app = FastAPI()
+
+# Create tables automatically
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"status": "backend is running"}
+
