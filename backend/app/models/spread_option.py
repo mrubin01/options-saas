52b759eb0bc8db3c655a156912a242c5994e7e1c
mrubin01 <mi.rubino@protonmail.com>
2025-12-13 08:55:47 +0000

    Create db model for spread options

--- backend/app/models/spread_option.py
+++ backend/app/models/spread_option.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey, Index
 from app.db.database import Base
 
-class PutOption(Base):
-    __tablename__ = "BEST_PUT_OPTIONS"
+class SpreadOption(Base):
+    __tablename__ = "BEST_SPREAD_OPTIONS"
 
     id = Column(String, primary_key=True, index=True)
     ticker = Column(String, nullable=False, index=True)
@@ -12,5 +12,5 @@ class PutOption(Base):
     strike_price = Column(Float, nullable=False)
 
     __table_args__ = (
-        Index("ix_put_options_ticker_expiry", "ticker", "expiry_date"),
+        Index("ix_spread_options_ticker_expiry", "ticker", "expiry_date"),
     )
