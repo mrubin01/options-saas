4e9c096f60c32d70eecbdbee89302e0814cb175a
mrubin01 <mi.rubino@protonmail.com>
2025-12-21 10:17:36 +0000

    Register the cov_call router in the main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.db.database import Base, engine
 from app import models
+from app.api.covered_calls import router as covered_calls_router
 
-app = FastAPI()
+app = FastAPI(title="Options SaaS API")
 
-# Create tables automatically
+# create tables automatically
 Base.metadata.create_all(bind=engine)
+
+# routers
+app.include_router(covered_calls_router)
 
 @app.get("/")
 def root():
