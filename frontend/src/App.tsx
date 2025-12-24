9fc5d5f9158f9e2487e79b7c114e2322f017e445
mrubin01 <mi.rubino@protonmail.com>
2025-12-24 11:25:36 +0000

    Wire spread options page into the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import CoveredCallsPage from "./pages/CoveredCallsPage";
 import PutOptionsPage from "./pages/PutOptionsPage";
 import Navigation from "./components/Navigation";
+import SpreadOptionsPage from "./pages/SpreadOptionsPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Navigate to="/covered-calls" />} />
         <Route path="/covered-calls" element={<CoveredCallsPage />} />
         <Route path="/put-options" element={<PutOptionsPage />} />
+        <Route path="/spread-options" element={<SpreadOptionsPage />} />
       </Routes>
     </>
   );
