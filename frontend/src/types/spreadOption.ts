41e65446924dc6a5332ad92d8007a88f39364ae7
mrubin01 <mi.rubino@protonmail.com>
2025-12-24 11:08:56 +0000

    Define type for spread options

---
+++ frontend/src/types/spreadOption.ts
@@ -0,0 +1,8 @@
+export interface SpreadOption {
+  contract: string;
+  ticker: string;
+  exchange: number;
+  expiry_date: string;      // ISO string from backend
+  current_price: number;
+  strike_price: number;
+}
