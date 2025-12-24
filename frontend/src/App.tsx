306565e919c6e07d0e5fbf2c84ffc7645e422c7e
mrubin01 <mi.rubino@protonmail.com>
2025-12-24 10:29:58 +0000

    Add routes to the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import CoveredCallsPage from "./pages/CoveredCallsPage";
 import PutOptionsPage from "./pages/PutOptionsPage";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <PutOptionsPage />;
+  return (
+    <>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/covered-calls" />} />
+        <Route path="/covered-calls" element={<CoveredCallsPage />} />
+        <Route path="/put-options" element={<PutOptionsPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
