5bff42d8cd2bc0cb4a34ecaebf1aa4d503a1a536
mrubin01 <mi.rubino@protonmail.com>
2025-12-28 10:49:06 +0000

    Fix auth router path

--- backend/app/auth/deps.py
+++ backend/app/auth/deps.py
@@ -7,7 +7,7 @@ from app.db.database import get_db
 from app.models.user import User
 from app.core.security import SECRET_KEY, ALGORITHM
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_current_user(
     token: str = Depends(oauth2_scheme),
