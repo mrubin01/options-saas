d3806a4d27dabcfae9192b8fcd71d58829e02c72
mrubin01 <mi.rubino@protonmail.com>
2025-12-29 11:05:41 +0000

    Show user during navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,17 +1,18 @@
-import { useAuth } from "../auth/AuthContext";
 import { Link } from "react-router-dom";
-
+import { useAuth } from "../auth/AuthContext";
 
 export default function Navigation() {
-  const { token, logout } = useAuth();
+  const { user, logout } = useAuth();
 
-  if (!token) return null;
+  if (!user) return null;
 
   return (
     <nav>
+      <span>{user.email}</span>
+
       <Link to="/covered-calls">Covered Calls</Link>
       <Link to="/put-options">Put Options</Link>
-      <Link to="/spread-options">Spreads</Link>
+      <Link to="/spread-options">Spread Options</Link>
 
       <button onClick={logout}>Logout</button>
     </nav>
