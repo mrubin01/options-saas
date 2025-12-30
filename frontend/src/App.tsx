d49811e6463c33e186f74d5b9be32a62604d5175
mrubin01 <mi.rubino@protonmail.com>
2025-12-30 16:25:04 +0000

    Wrap pages with Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,44 +5,47 @@ import SpreadOptionsPage from "./pages/SpreadOptionsPage";
 import LoginPage from "./pages/LoginPage";
 import Navigation from "./components/Navigation";
 import { RequireAuth } from "./auth/RequireAuth";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <>
       <Navigation />
 
-      <Routes>
-        <Route path="/" element={<Navigate to="/covered-calls" />} />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/covered-calls" />} />
 
-        <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          path="/covered-calls"
-          element={
-            <RequireAuth>
-              <CoveredCallsPage />
-            </RequireAuth>
-          }
-        />
+          <Route
+            path="/covered-calls"
+            element={
+              <RequireAuth>
+                <CoveredCallsPage />
+              </RequireAuth>
+            }
+          />
 
-        <Route
-          path="/put-options"
-          element={
-            <RequireAuth>
-              <PutOptionsPage />
-            </RequireAuth>
-          }
-        />
+          <Route
+            path="/put-options"
+            element={
+              <RequireAuth>
+                <PutOptionsPage />
+              </RequireAuth>
+            }
+          />
 
-        <Route
-          path="/spread-options"
-          element={
-            <RequireAuth>
-              <SpreadOptionsPage />
-            </RequireAuth>
-          }
-        />
-      </Routes>
+          <Route
+            path="/spread-options"
+            element={
+              <RequireAuth>
+                <SpreadOptionsPage />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </Layout>
     </>
   );
 }
