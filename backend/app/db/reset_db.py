d7220d4952dad7f1515a8ec3abb837552271e308
mrubin01 <mi.rubino@protonmail.com>
2025-12-31 10:03:27 +0000

    Import all models in reset_db

--- backend/app/db/reset_db.py
+++ backend/app/db/reset_db.py
@@ -1,4 +1,12 @@
 from app.db.database import engine, Base
+from app.models.covered_call import CoveredCall
+from app.models.put_option import PutOption
+from app.models.spread_option import SpreadOption
+from app.models.exchange import Exchange
+from app.models.trend import Trend
+from app.models.sector import Sector
+from app.models.industry import Industry
+from app.models.user import User
 
 def reset_db():
     print("Dropping all tables...")
