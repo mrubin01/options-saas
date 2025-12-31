1664c31df6d9175f80523324a1ede5205338cc20
mrubin01 <mi.rubino@protonmail.com>
2025-12-31 09:49:55 +0000

    Create file to reset db

---
+++ backend/app/db/reset_db.py
@@ -0,0 +1,11 @@
+from app.db.database import engine, Base
+
+def reset_db():
+    print("Dropping all tables...")
+    Base.metadata.drop_all(bind=engine)
+
+    print("Creating all tables...")
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    reset_db()
