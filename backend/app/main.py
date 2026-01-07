03bca7f5d1562bd8eb5565bd6c208c9ba86a8898
mrubin01 <mi.rubino@protonmail.com>
2026-01-07 22:19:00 +0000

    Fix CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,7 +42,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=[
         "Authorization",
         "Content-Type",
