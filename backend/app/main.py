604bdafbace873a0168b6264c0c190447bd5c6c4
mrubin01 <mi.rubino@protonmail.com>
2026-01-08 22:16:33 +0000

    Update middleware allowing headers

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,10 +43,7 @@ app.add_middleware(
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=[
-        "Authorization",
-        "Content-Type",
-    ],
+    allow_headers=["*"],
 )
 
 # create tables automatically TEMPORARY
